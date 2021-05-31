721564eaac00ec17cd2f25260a7d07e6c97dc7a4
daniprec <danipg1409@gmail.com>
2021-05-31 13:18:41 +0200

    Move some Analysis methods to new object Series

--- neocam/utils/analysis.py
+++ neocam/utils/analysis.py
@@ -3,57 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-from matplotlib.lines import Line2D
 
-from neocam.utils.series import moving_average
+from neocam.utils.series import Series
 
 
 class Analysis:
     fig: Figure
     ax: Axes
-    line1: Line2D
-    line2: Line2D
 
     def __init__(self, size: int = 1000, frequency: int = 24):
         # Initialize series
-        ser = np.empty(size)
-        ser[:] = np.nan
-        self.ser_right = ser
-        self.ser_left = ser
-        self.ser_up = ser
-        self.ser_down = ser
+        self.ser_right = Series(size=size, frequency=frequency, label="Right")
+        self.ser_left = Series(size=size, frequency=frequency, label="Left")
+        self.ser_up = Series(size=size, frequency=frequency, label="Up")
+        self.ser_down = Series(size=size, frequency=frequency, label="Down")
         # Plot
         self.frequency = frequency
         self._timer = 0
         self._initialize_plot(size)
 
-    def __len__(self):
-        return len(self.ser_right)
-
-    @property
-    def ser_right_movavg(self) -> np.array:
-        return moving_average(self.ser_right, size=self.frequency)
-
-    @property
-    def ser_left_movavg(self) -> np.array:
-        return moving_average(self.ser_left, size=self.frequency)
-
-    @property
-    def ser_up_movavg(self) -> np.array:
-        return moving_average(self.ser_up, size=self.frequency)
-
-    @property
-    def ser_down_movavg(self) -> np.array:
-        return moving_average(self.ser_down, size=self.frequency)
-
     def _initialize_plot(self, size: int):
         """Initializes the plot"""
         self.fig, self.ax = plt.subplots()
         self.fig.canvas.draw()
-        (self.line1,) = self.ax.plot(self.ser_right, label="Right")
-        (self.line2,) = self.ax.plot(self.ser_left, label="Left")
-        (self.line3,) = self.ax.plot(self.ser_up, label="Up")
-        (self.line4,) = self.ax.plot(self.ser_down, label="Down")
+        self.ser_right.plot(self.ax)
+        self.ser_left.plot(self.ax)
+        self.ser_up.plot(self.ax)
+        self.ser_down.plot(self.ax)
         self.ax.grid()
         self.ax.set_xlabel("Time (frames)")
         self.ax.set_ylabel("Detection box size")
@@ -64,10 +40,10 @@ class Analysis:
 
     def _update_plot(self):
         """Updates the plot lines"""
-        self.line1.set_ydata(self.ser_right_movavg)
-        self.line2.set_ydata(self.ser_left_movavg)
-        self.line3.set_ydata(self.ser_up_movavg)
-        self.line4.set_ydata(self.ser_down_movavg)
+        self.ser_right.update_plot()
+        self.ser_left.update_plot()
+        self.ser_up.update_plot()
+        self.ser_down.update_plot()
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
@@ -90,10 +66,10 @@ class Analysis:
             # If not, the size is NaN
             right, left, up, down = np.nan, np.nan, np.nan, np.nan
         # Update the series of box sizes
-        self.ser_right = np.append(self.ser_right[1:], right)
-        self.ser_left = np.append(self.ser_left[1:], left)
-        self.ser_up = np.append(self.ser_up[1:], up)
-        self.ser_down = np.append(self.ser_down[1:], down)
+        self.ser_right.append(right)
+        self.ser_left.append(left)
+        self.ser_up.append(up)
+        self.ser_down.append(down)
         # Plot the evolution of box size
         self._timer += 1
         if self._timer >= self.frequency:
--- neocam/utils/series.py
+++ neocam/utils/series.py
@@ -1,4 +1,6 @@
 import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.lines import Line2D
 
 
 def moving_average(ser: np.array, size: int = 24) -> np.array:
@@ -15,4 +17,43 @@ def moving_average(ser: np.array, size: int = 24) -> np.array:
     numpy.array
     """
     ret = np.convolve(ser, np.ones(size), "valid") / size
-    return np.append(ser[: (size - 1)], ret)
+    return np.append(np.repeat(np.nan, size - 1), ret)
+
+
+class Series:
+    line: Line2D = None
+
+    def __init__(self, size: int = 1000, frequency: int = 24, label: str = ""):
+        """Series
+
+        Parameters
+        ----------
+        size : int, optional
+            Length of the series, by default 1000
+        frequency : int, optional
+            Size of the moving average, by default 24
+        label : str, optional
+            Name of the series, by default ""
+        """
+        self.ser = np.empty(size)
+        self.ser[:] = np.nan
+        self.frequency = frequency
+        self.label = label
+
+    def __len__(self):
+        return len(self.ser)
+
+    def append(self, x: float):
+        self.ser = np.append(self.ser[1:], x)
+
+    @property
+    def movavg(self):
+        return moving_average(self.ser, size=self.frequency)
+
+    def plot(self, ax: Axes):
+        """Plots the line of given axes"""
+        (self.line,) = ax.plot(self.ser, label=self.label)
+
+    def update_plot(self):
+        """Updates the plotted line"""
+        self.line.set_ydata(self.movavg)
