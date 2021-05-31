df4cdeee850294395995553721cd1e3e0f81a382
daniprec <danipg1409@gmail.com>
2021-05-31 14:00:32 +0200

    Move anonymize function outside display frame

--- neocam/utils/frame.py
+++ neocam/utils/frame.py
@@ -16,38 +16,43 @@ def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
     return cv2.resize(arr, shape).transpose(2, 0, 1).flatten()
 
 
+def anonymize(frame, bbox, anonymize_method):
+    # Locate the face
+    y1 = int(1.3 * bbox[1])
+    y2 = int(bbox[1] + 0.2 * (bbox[3] - bbox[1]))
+    x1 = int(bbox[0])
+    x2 = int(bbox[0] + 0.7 * (bbox[2] - bbox[0]))
+
+    if anonymize_method == "blur":
+        face = frame[y1:y2, x1:x2]
+        face = anonymize_shape_simple(face, factor=3.0)
+        frame[y1:y2, x1:x2] = face
+        # otherwise, we must be applying the "filled" face
+        # anonymization method
+
+    elif anonymize_method == "pixelate":
+        face = frame[y1:y2, x1:x2]
+        face = anonymize_shape_pixelate(face, blocks=3)
+        frame[y1:y2, x1:x2] = face
+        # otherwise, we must be applying the "filled" face
+        # anonymization method
+    elif anonymize_method == "filled":
+        cv2.rectangle(
+            frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 255, 255), -1
+        )
+
+    return frame
+
+
 def display_frame(name: str, frame, detections: list, anonymize_method: str = None):
     """Displays the frame on screen"""
     for detection in detections:
         bbox = frame_norm(
             frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax)
         )
-        # Locate the face
-        y1 = int(1.3 * bbox[1])
-        y2 = int(bbox[1] + 0.2 * (bbox[3] - bbox[1]))
-        x1 = int(bbox[0])
-        x2 = int(bbox[0] + 0.7 * (bbox[2] - bbox[0]))
 
-        if anonymize_method == "blur":
-            face = frame[y1:y2, x1:x2]
-            face = anonymize_shape_simple(face, factor=3.0)
-            frame[y1:y2, x1:x2] = face
-            # otherwise, we must be applying the "filled" face
-            # anonymization method
-
-        elif anonymize_method == "pixelate":
-            face = frame[y1:y2, x1:x2]
-            face = anonymize_shape_pixelate(face, blocks=3)
-            frame[y1:y2, x1:x2] = face
-            # otherwise, we must be applying the "filled" face
-            # anonymization method
-        elif anonymize_method == "filled":
-            cv2.rectangle(
-                frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 255, 255), -1
-            )
-
-        else:
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+        frame = anonymize(frame, bbox, anonymize_method)
+        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
 
         cv2.putText(
             frame,
