cb013b173fe0e963194b6215b0c7ac1a4ee117c6
Daniel Precioso <danipg1409@gmail.com>
2021-06-09 10:33:41 +0200

    Script to test Analysis class

---
+++ test/test_analysis.py
@@ -0,0 +1,22 @@
+import os
+
+from neocam.utils.analysis import Analysis
+from neocam.utils.json import read_json
+
+from unittest import TestCase
+
+
+def test_analysis_to_json():
+    path_json = "test.json"
+    analysis = Analysis(size=240, frequency=1, plot_series=False, dummy=False)
+    analysis.to_json(path_json)
+    # Read the json we just stored
+    dict_json = read_json(path_json)
+    os.remove(path_json)
+    # Assert dictionaries are equal
+    case = TestCase()
+    case.assertDictEqual(dict_json, analysis.dict)
+
+
+if __name__ == "__main__":
+    test_analysis_to_json()
